b01b80fbb1842acf280629c560d9799ef7925d90
alimrad <alimrad@gmail.com>
2021-11-14 10:55:10 -0800

    update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
-window.addEventListener('scroll', function () {
-    let header = document.querySelector('nav');
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle('scrolling-active', windowPosition);
-})
+console.log("js file is excuting");
+var nav = document.getElementById('nav'); // Identify target
+
+window.addEventListener('scroll', function(event) { // To listen for event
+    event.preventDefault();
+
+    if (window.scrollY >= 80) { // Just an example
+        nav.style.backgroundColor = '#ddddff'; // or default color
+    } else {
+        nav.style.backgroundColor = 'transparent';
+    }
+});
